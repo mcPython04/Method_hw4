3e878129f48d9505c5925da0e1fab741ebe63215
Jinhee Yu <77952232+jy603@users.noreply.github.com>
2021-10-22 10:17:46 -0500

    Update functions.py
    edied displayItem() with try and except statement

--- functions.py
+++ functions.py
@@ -73,4 +73,8 @@ def greetUser(first, middle, last):
 ## takes in a Python list
 ## attempts to display the item at the index provided
 def displayItem(numbers, index):
-    print("Your item at", index, "index is", numbers[index])
+    try:
+        print("Your item at", index, "index is", numbers[index])        
+    except:
+        print("Please put the right inputs")
+    
